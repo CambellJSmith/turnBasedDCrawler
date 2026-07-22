fb767c20f515c1f868424e121c28bc8db6502920
Animatic_Almanac <67105502+CambellJSmith@users.noreply.github.com>
2026-07-22 15:43:13 +0100

    Persist recruited monster party members

--- ashfall_depths/src/state/party_state.js
+++ ashfall_depths/src/state/party_state.js
@@ -2,6 +2,7 @@ export class PartyState {
   constructor() {
     this.member_ids = ["hero"];
     this.maximum_members = 3;
+    this.monster_members = {};
   }
 
   add_member(character_id) {
@@ -12,11 +13,31 @@ export class PartyState {
     return true;
   }
 
+  add_monster_member(member) {
+    if (!member?.member_id || !member.monster_id || !this.add_member(member.member_id)) {
+      return false;
+    }
+    this.monster_members[member.member_id] = { ...member };
+    return true;
+  }
+
+  get_monster_member(member_id) {
+    return this.monster_members[member_id] ?? null;
+  }
+
   to_json() {
-    return { member_ids: [...this.member_ids] };
+    return {
+      member_ids: [...this.member_ids],
+      monster_members: Object.fromEntries(
+        Object.entries(this.monster_members).map(([member_id, member]) => [member_id, { ...member }])
+      )
+    };
   }
 
   load(data) {
     this.member_ids = Array.isArray(data?.member_ids) && data.member_ids.length > 0 ? [...data.member_ids] : ["hero"];
+    this.monster_members = data?.monster_members && typeof data.monster_members === "object"
+      ? Object.fromEntries(Object.entries(data.monster_members).map(([member_id, member]) => [member_id, { ...member }]))
+      : {};
   }
 }
